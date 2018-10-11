7fefe0715a0e4e97201504a0d4d6fd27a603acac
a lan <bluenowte97@gmail.com>
2018-10-11 00:21:24 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -20,11 +20,11 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string){
 
-  if (string.toLowerCase === lowercase) {
+  if (string.toLowerCase() === lowercase) {
     return 'I' + "can't"  + 'hear you';
   }
 
-  if (string.toUpperCase === uppercase) {
+  if (string.toUpperCase() === uppercase) {
     return "YES INDEED!";
   }
   if (string === "I love you, Grandma"){
