cb5e0c6dae93e47daf35e7a566f1b838bcd4ceb8
a lan <bluenowte97@gmail.com>
2018-10-11 00:05:30 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,3 +1,11 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+
+
+
+
 
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
