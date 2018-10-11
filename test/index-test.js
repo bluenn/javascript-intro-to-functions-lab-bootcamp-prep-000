2b42ffd64c9d756ed63a02d11df69396797e7722
a lan <bluenowte97@gmail.com>
2018-10-11 00:20:18 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -21,7 +21,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string){
 
   if (string.toLowerCase ===lowercase) {
-    return 'I' + "can't"  + 'hear you';    
+    return 'I' + "can't"  + 'hear you';
   }
 
   if (string.toUpperCase ===uppercase) {
